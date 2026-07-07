afdb369d950eebc807334f4085828986107046fb
kj <kj@DESKTOP-O9OTGCG>
2026-07-07 11:14:03 +0900

    movie

--- java/src/chapter5/영화관.java
+++ java/src/chapter5/영화관.java
@@ -11,10 +11,21 @@ public class 영화관 {
 		Scanner sc = new Scanner(System.in);
 		Random r = new Random();
 
+
 		System.out.println("             §§§ 영화관 §§§");
 		System.out.println();
 		System.out.println();
-
+		System.out.println("영화를 선택해주세요");
+		System.out.println("1. 티파니에서 아침을       2. 석류의 빛깔       3. 버닝");
+		System.out.print("선택: ");
+		int movie = sc.nextInt();
+		
+		String[] moviename = {"티파니에서 아침을", "석류의 빛깔", "버닝"};
+		
+		System.out.println(moviename[movie-1] + " 선택하셨습니다");
+		System.out.println();
+		System.out.println();
+		
 		boolean boo = true;
 		while (boo = true) {
 
@@ -28,9 +39,7 @@ public class 영화관 {
 					sit[i][j] = '□';
 				}
 			}
-
-			String[] A = { "예약가능", "예약불가" };
-
+			
 			for (int k = 0; k < 8; k++) {
 				int w = r.nextInt(0, 9);
 				int h = r.nextInt(0, 4);
@@ -56,22 +65,23 @@ public class 영화관 {
 			System.out.println();
 			System.out.println("좌석을 선택해주세요 (A1 ~ J5)");
 
-		
+			String[] seatList = new String[answer];
 						
-			int alp = 0; 
-			int sit_Array = 0;
-			int answer2 = 0;
-			int i = 0;
-			int price = 0;
-			int AclassCnt = 0;
+			int alp = 0; 			// 행을 알파벳으로 입력받고 숫자로 바꾸기 위해 선언
+			int sit_Array = 0;		// sit[answer2-1(열)][alp(행)]를 저장해놓기 위해 선언
+			int answer2 = 0;		// 열
+			int i = 0;				// 아래의 while문을 돌리기 위해 선언한 변수 
+			int price = 0;			// 가격을 계산하기 위해 선언
+			int AclassCnt = 0;		// A구역, B구역, C구역을 몇 개씩 예매했는지 체크하기 위해 선언 (가격계산때문에)
 			int BclassCnt = 0;
 			int CclassCnt = 0;
+	
 			
-			int[] height = new int[5];
-			int[] weight = new int[10];
+			int[] height = new int[5];		//5열의 배열 생성
+			int[] weight = new int[10];		//10행의 배열 생성 
 			
 			
-			while (i < answer) {
+			while (i < answer) {				
 				System.out.print("행(A~J): ");
 				String answer1 = sc.next();
 				
@@ -119,18 +129,25 @@ public class 영화관 {
 				
 				System.out.print("열(1~5): ");
 				answer2 = sc.nextInt();
-	
+		
+				sit_Array = sit[answer2 - 1][alp];
+				if (sit_Array == '■') {
+					System.out.println("예약 불가능한 좌석입니다");
+					continue;
+				} // if문의 continue를 사용하여 입력하는 사람이 중복된 좌석을 고를 시 다시 조건문 시작 부분으로 돌아가게 함
 				
+				
+
 				height[i] = answer2 - 1;
 				weight[i] = alp;	
-				i++;
-				sit_Array = sit[answer2 - 1][alp];
-				while (sit_Array == '■') {
-					System.out.println("예약 불가능한 좌석입니다");
-					i--;
-					break;
-				} 
+				seatList[i] = answer1 + answer2;
+				i++; // n번째 돌렸을 때의 열과 행의 값을 저장해놓음 여러개를 저장해야 할수도 있어서 배열로
+				
+				
+				// 1열-2열 : A구열, 3열-4열 : B구역, 5열 - C구역으로 정함
+				// 입력된 좌석이 A, B, C구역인지 확인하기 위해 if문 사용
 			
+				
 				if(answer2 < 3 && alp <= 10) {
 					System.out.println("A구역 좌석을 선택하셨습니다. (15000원)");
 							AclassCnt++;
@@ -145,7 +162,6 @@ public class 영화관 {
 				price = AclassCnt*15000 + BclassCnt*13000 + CclassCnt*11000;
 			
 			}
-			
 			System.out.println();
 			System.out.println("예약하시겠습니까?");
 			System.out.print("1. 예     2. 아니오: ");
@@ -164,7 +180,7 @@ public class 영화관 {
 					System.out.print("지불할 현금 입력: ");
 					int money = sc.nextInt();
 					if(money < price) {System.out.println("금액이 부족합니다.");
-					break;
+					continue;
 					}  else if(money>=price) {System.out.println(money + " 원 받았습니다");
 					System.out.println("거스름돈: " + (money - price));
 				} 
@@ -176,11 +192,12 @@ public class 영화관 {
 				System.out.println("----------------------------------------");
 				System.out.printf("%s%3s %3s %3s %3s %3s %3s %3s %3s %3s %3s\n",
 						" ", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J");
-		
+						
 				for (int k = 0; k < answer; k++) {
 					sit[height[k]][weight[k]] = '■';
-				}
-
+				}	// 선택된 좌석들을 ■로 표시하기 위해 for문 선언
+				
+				
 				for (int x = 0; x < 5; x++) {
 					for (int y = x; y == x; y++) {
 						System.out.print(x + 1);
@@ -190,7 +207,7 @@ public class 영화관 {
 						System.out.printf("%3c ", sit[x][z]);
 					}
 					System.out.println();
-				}
+				} // 좌석 선택창을 한 번 더 띄우기 위해 위에서 사용했던 for문을 한번 더 사용함
 			}
 
 			if (yes == 2) {
@@ -202,9 +219,19 @@ public class 영화관 {
 				continue;
 			}
 			System.out.println();
-			System.out.println("-----♡♡♡♡-----");
-			System.out.println("즐거운 관람 되세요");
-			System.out.println("-----♡♡♡♡-----");
+			System.out.println("-----------------------------------------------------------------");
+			System.out.println("                        ♧   " + moviename[movie-1] + "   ♧");
+			System.out.println("                        인원: " + answer + "명");       
+			System.out.println("                        가격: " + price + " 원");
+			System.out.print("                        좌석: ");
+			for(int a = 0; a < answer; a++) {
+				System.out.print(seatList[a] + " ");
+			}	System.out.println();	
+			System.out.println();
+			System.out.println("                       ♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥");
+			System.out.println("                         즐거운 관람 되세요 :-)");
+			System.out.println("                       ♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥♡♥");          
+			System.out.println("-----------------------------------------------------------------");
 			break;
 			
 
