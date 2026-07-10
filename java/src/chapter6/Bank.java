9a8cefb1f2173642cc70c192ec553d07cd006bd5
0904heimish <0904heimish@gmail.com>
2026-07-10 11:29:23 +0900

    1

--- java/src/chapter6/Bank.java
+++ java/src/chapter6/Bank.java
@@ -3,13 +3,14 @@ package chapter6;
 import java.util.Scanner;
 
 public class Bank {
-
+	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
 		// 정보 담을 변수
 		String[][] info = new String[100][3];
 		int a = 0;
+		int b = 0;
 
 		boolean run = true;
 		
@@ -18,19 +19,24 @@ public class Bank {
 			int button = sc.nextInt();
 			
 				if(button == 1) {
+					a = person(info, a);
 					
 				}
 				
 				else if(button == 2) {
+					accountList(info, a);
 					
 				}
 				
 				else if(button == 3) {
-					System.out.println("3");
-				}
+					System.out.println("얼마를 입금하시겠습니까");
+					b = sc.nextInt();
+					int result1 = sum(info, a, b);
+					System.out.println("잔액: ");
+				}	
 				
 				else if(button == 4) {
-					System.out.println("4");
+					
 				}
 				
 				else if(button == 5) {
@@ -42,9 +48,6 @@ public class Bank {
 					break;
 				} 
 		}
-	
-		
-		
 	
 	}
 	
@@ -63,26 +66,48 @@ public class Bank {
 	
 	
 	
-	public static void person(String[][] info, int a) {
+	public static int person(String[][] info, int a) {
 		Scanner sc = new Scanner(System.in);
-		a = 0;
 		
 		
 		System.out.println("이름을 입력하세요");
 		String name = sc.next();
+		info[a][0] = name;
 
 		System.out.println("계좌번호를 입력하세요");
-		String account = sc.next();
+	
+			for(int i = 0; i < a; i++) {
+				String account = sc.next();
+				info[i][1] = account;
+					for(int j = 0; j < i; j++) {
+						if(info[i][1] == info[j][1]) {
+							System.out.println("중복된 계좌입니다");
+						} else {
+							System.out.println("이름: " + info[a][0]);
+							System.out.println("계좌번호: " + info[a][1]);
+							a++;	
+						
+					}		
+			
+			}
 
-			info[a][0] = name;
-			info[a][1] = account;
-			a++;
-
-			System.out.println("이름: " + info[a][0]);
-			System.out.println("계좌번호: " + info[a][1]);
-			return;
+	
+			}
+			return a;
+			
 		
 	}
 
+	public static void accountList(String[][] info, int a) {
+		for(int i = 0; i < a; i++) {
+			System.out.println(i+1 + ". " + info[i][1]);
+		}		
+	}
+	
+	static int sum(String[][] info, int a, int b) {
+		
+		int result = Integer.parseInt(info[a][2]) + b;		
+		return result;
+	}
 	
 }
