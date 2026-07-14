0453ce4d728115f6a16baa6384cead2a04b130dc
0904heimish <0904heimish@gmail.com>
2026-07-14 11:14:23 +0900

    12321321

--- java/src/chapter7/AirConditioner.java
+++ java/src/chapter7/AirConditioner.java
@@ -1,21 +1,34 @@
 package chapter7;
 
 public class AirConditioner extends Remote {
-	int temp;
+	int temp = 20;
 	
 	void powerOn(int temp) {
 		System.out.println("에어컨이 켜졌습니다.");
 		System.out.println("현재 설정된 온도는 " + this.temp + "도 입니다.");
+		this.function = true;
 	}
 		
-	void tempUp(int temp) {
+	void tempUp(int temp) {;
+		this.temp++;
+		if(this.temp > 28) {
+			System.out.println("최대 온도 설정입니다");
+			this.temp--;
+		} else {
 		System.out.println("온도를 올립니다.");
 		System.out.println("현재 온도는 " + this.temp + "도 입니다.");
+		}
 	}
 	
 	void tempDown(int temp) {
+		this.temp--;
+		if(this.temp < 18) {
+			System.out.println("최저 온도 설정입니다");
+			this.temp++;
+		} else {
 		System.out.println("온도를 내립니다.");
 		System.out.println("현재 온도는 " + this.temp + "도 입니다.");
+		}
 	}
 	
 }
--- java/src/chapter7/Light.java
+++ java/src/chapter7/Light.java
@@ -3,17 +3,35 @@ package chapter7;
 import java.util.Scanner;
 
 public class Light extends Remote {
-	int lightLevel;
+	int lightLevel = 1;
 	Scanner sc = new Scanner(System.in);
 	
-
+	
 	void powerOn (int lightLevel) {
 		System.out.println("전등이 켜졌습니다");
+		this.lightLevel++;
 		System.out.println("현재 밝기는 " + this.lightLevel + "단계 입니다.");
 	}
 	
-	void lightLevel(int lightLevel) {
+	void lightLevelUp(int lightLevel) {
+		this.lightLevel++;
+		if(this.lightLevel > 7) {
+			System.out.println("더 올릴 수 없습니다");
+			this.lightLevel--;
+		} else {
 		System.out.println("현재 밝기는 " + this.lightLevel + "단계 입니다.");
+		}
+	}
+	
+	
+	void lightLevelDown(int lightLevel) {
+		this.lightLevel--;
+		if(this.lightLevel < 1) {
+			System.out.println("더 내릴 수 없습니다");
+			this.lightLevel++;
+		} else {
+		System.out.println("현재 밝기는 " + this.lightLevel + "단계 입니다.");
+		}
 	}
 	
 }
