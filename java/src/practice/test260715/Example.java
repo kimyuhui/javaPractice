d168c72b539e9de84ae5f992b3e1e10649aa100a
0904heimish <0904heimish@gmail.com>
2026-07-16 16:52:12 +0900

    homework

--- java/src/practice/test260715/Example.java
+++ java/src/practice/test260715/Example.java
@@ -8,48 +8,43 @@ public class Example {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
+
 		Dice dice = new Dice();
 		DangerDice dangerdice = new DangerDice();
 		SafeDice safedice = new SafeDice();
 		SosoDice sosodice = new SosoDice();
-		
+
 		String computerDice;
 		String computerDiceResult;
 		String computerDiceSum;
 
-		
 		int userDice; // 유저가 선택할 주사위 숫자
 
 		int userDiceSum = 0;
-		
+
 		int count = 0;
 		System.out.println("주사위 고르세요");
-		
-		
+
 		while(userDiceSum < gameScore) {
-			
-			
-		for(int i = 0; i < 3; i++) {
-			userDice = Integer.parseInt(sc.next());
-			if(userDice == 1) {
-				userDice = dice.roll();
-			} else if (userDice == 2) {
-				userDice = dangerdice.roll();
-			} else if (userDice == 3) {
-				userDice = safedice.roll();
-			} else if (userDice == 4) {
-				userDice = sosodice.roll();
-			}
-			for(int j = 0; j == 0; j++) {
-				userDiceSum += userDice;
-				System.out.println("합계: " + userDiceSum);
-			} 
-		} count++;
-		System.out.println(count + "번 끝 ");
-		
-		
-	}
+
+			for(int i = 0; i < 3; i++) {
+				userDice = Integer.parseInt(sc.next());
+				if(userDice == 1) {
+					userDice = dice.roll();
+				} else if (userDice == 2) {
+					userDice = dangerdice.roll();
+				} else if (userDice == 3) {
+					userDice = safedice.roll();
+				} else if (userDice == 4) {
+					userDice = sosodice.roll();
+				}
+				for(int j = 0; j == 0; j++) {
+					userDiceSum += userDice;
+					System.out.println("합계: " + userDiceSum);
+				} 
+			} count++;
+			System.out.println(count + "번 끝 ");
+		}
 	}
 
 }
